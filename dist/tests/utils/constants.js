468f54e1437a7eef8216114d022e2b3a4afa26d5
Vishwas <vishwasanand.cse@gmail.com>
2022-06-30 10:38:44 +0530

    removed redundatant folder

--- dist/tests/utils/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HIDNODE_REST = exports.HIDNODE_RPC = exports.mnemonic = void 0;
-exports.mnemonic = "retreat seek south invite fall eager engage endorse inquiry sample salad evidence express actor hidden fence anchor crowd two now convince convince park bag";
-exports.HIDNODE_RPC = "http://localhost:26657";
-exports.HIDNODE_REST = "http://localhost:1317";
--- tests/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-
-export const mnemonic = "retreat seek south invite fall eager engage endorse inquiry sample salad evidence express actor hidden fence anchor crowd two now convince convince park bag"
-export const HIDNODE_RPC="http://localhost:26657" 
-export const HIDNODE_REST="http://localhost:1317"
