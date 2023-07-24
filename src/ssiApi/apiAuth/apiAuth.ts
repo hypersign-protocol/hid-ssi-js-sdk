45a5c54fa269e88174a01f835b0e7b43f5e0046c
varsha766 <varshakumari370@gmail.com>
2023-07-24 10:34:42 +0530

    code prettified

--- src/ssiApi/apiAuth/apiAuth.ts
+++ src/ssiApi/apiAuth/apiAuth.ts
@@ -1,30 +1,28 @@
-import fetch from "node-fetch"
-import { IgenerateToken } from "./IAuth";
-import {APIENDPOINT} from "../api-constant"
+import fetch from 'node-fetch';
+import { IgenerateToken } from './IAuth';
+import { APIENDPOINT } from '../api-constant';
 export class ApiAuth {
-    private apiKey: string;
-    constructor(apiKey) {
-        if (!apiKey || apiKey.trim() === "") {
-            throw new Error("HID-SSI_SDK:: Error: Please Provide apiKey")
-        }
-        this.apiKey = apiKey
+  private apiKey: string;
+  constructor(apiKey) {
+    if (!apiKey || apiKey.trim() === '') {
+      throw new Error('HID-SSI_SDK:: Error: Please Provide apiKey');
     }
-    async generateAccessToken(): Promise<IgenerateToken> {
-            const studioApiUrl = `${APIENDPOINT.STUDIO_API_BASE_URL}${APIENDPOINT.AUTH}`
-            const headers = {
-                "X-Api-Secret-Key": this.apiKey,
-            }
-            const requestOptions = {
-                method: "POST",
-                headers,
-            }
-            const response = await fetch(studioApiUrl, requestOptions)
-            const authToken = await response.json()
-            if (!response.ok) {
-                throw new Error(`HID-SSI-SDK:: Error: ${authToken.message}`)
-            }
-            return authToken
-       
-       
+    this.apiKey = apiKey;
+  }
+  async generateAccessToken(): Promise<IgenerateToken> {
+    const studioApiUrl = `${APIENDPOINT.STUDIO_API_BASE_URL}${APIENDPOINT.AUTH}`;
+    const headers = {
+      'X-Api-Secret-Key': this.apiKey,
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers,
+    };
+    const response = await fetch(studioApiUrl, requestOptions);
+    const authToken = await response.json();
+    if (!response.ok) {
+      throw new Error(`HID-SSI-SDK:: Error: ${authToken.message}`);
     }
+    return authToken;
+  }
 }
